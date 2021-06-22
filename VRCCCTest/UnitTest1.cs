250935fae4a57253bd452cae46ed065165c90329
InconsolableCellist <user@user.com>
2021-06-21 23:46:59 -0700

    Adding unit tests in VRCCCTest project

---
+++ VRCCCTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using static VRCCC.SubtitlesApi;
+
+namespace VRCCCTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task TestFetch()
+        {
+            var result = await FetchSub("https://dl.opensubtitles.org/en/download/src-api/vrf-19a80c53/filead/1955211963.gz");
+            Console.WriteLine(result);
+            Assert.True(result.Length > 512);
+        }
+    }
+}
