5a233d61c7458f23667560cbc0d13ab39329de71
InconsolableCellist <user@user.com>
2021-07-21 19:44:58 -0600

    Added offset, search, code clean-up

--- VRCCC/QuickMenu/MainMenu.cs
+++ VRCCC/QuickMenu/MainMenu.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using MelonLoader;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using VRC.SDKBase.Validation.Performance.Scanners;
 using Object = UnityEngine.Object;
 
 namespace VRCCC.QuickMenu
@@ -25,8 +25,10 @@ namespace VRCCC.QuickMenu
         private InputField _inputField;
         private Subtitle _subtitle;
         
+        private bool _initSucc = false;
+        
+        
         public MainMenu(Transform parentMenuTransform, AssetBundle bundle) { 
-            // Grab prefabs
             GameObject menuPrefab = bundle.LoadAsset<GameObject>("Assets/AssetBundles/VRCCC-UI.prefab");
             if (menuPrefab == null) { 
                 MelonLogger.Error("Failure when trying to get menuPrefab or resultPrefab");
@@ -39,12 +41,10 @@ namespace VRCCC.QuickMenu
             _menuObject.transform.localScale     = Vector3.oneVector;
             _menuObject.transform.localRotation  = new Quaternion(0, 0, 0, 1);
             
-            
             InitReferences();
             MelonLogger.Msg("UI: Grabbed references");
             SetupStaticButtons(); 
             MelonLogger.Msg("UI: set up buttons and finished init");
-            
         }
         
         private void InitReferences() { 
@@ -58,9 +58,9 @@ namespace VRCCC.QuickMenu
                     if (button.name == "PositiveS")     { _positiveSButton = button; }
                 }
                 Text[] texts = _menuObject.GetComponentsInChildren<Text>();
-                foreach (Text text in texts) { 
+                foreach (Text text in texts) 
                     if (text.name == "CurrentOffset")   { _currentOffsetText = text; }
-                }
+                
                 _inputField = _menuObject.GetComponentInChildren<InputField>();
                 _listContent = _menuObject.transform.Find(
                     "Pages/MainPage/ResultsArea/ScrollList/ListViewport/ListContent").gameObject;
@@ -68,10 +68,16 @@ namespace VRCCC.QuickMenu
                     "Pages/MainPage/ResultsArea/ScrollList/ListViewport/ListContent/ResultObject").gameObject;
             } catch (Exception e) { 
                 MelonLogger.Error($"Error while trying to get individual components in the menu prefab. {e}.");
+                return;
             }
-            if (_searchButton == null) MelonLogger.Error("Unable to get Search Button");
-            if (_listContent == null) MelonLogger.Error("Unable to get the list content GameObject");
-            if (_resultItem == null) MelonLogger.Error("Unable to get the result item");
+            // Init complete
+            //
+            // Verify init
+            if (_searchButton == null) { MelonLogger.Error("Unable to get Search Button"); return; }
+            if (_listContent == null) { MelonLogger.Error("Unable to get the list content GameObject"); return; }
+            if (_resultItem == null) { MelonLogger.Error("Unable to get the result item"); return; }
+            
+            _initSucc = true;
         }
         
         /**
@@ -80,48 +86,56 @@ namespace VRCCC.QuickMenu
          * objects that display the results and lets the user change to a new subtitle.
          */
         private void SetupStaticButtons() { 
-            _searchButton.onClick.AddListener((Action)(async () => { 
-                if (_inputField.text != "") { 
-                    // TODO: fix clearing previous list 
-                    //ClearResultList()
-                    MelonLogger.Msg($"Searching for {_inputField.text}");
-                    try {
-                        _subtitle = await SubtitlesApi.QuerySubtitle(_inputField.text, true);
-                        MelonLogger.Msg($"{_subtitle.Alternatives.Count} results in the list of alternatives.");
-                        foreach (Subtitle subtitle in _subtitle.Alternatives.Take(3)) { 
-                            if (subtitle != null) { 
-                                MelonLogger.Msg($"Handling returned result for {subtitle.MovieName}.");
-                                try { 
-                                    GameObject result = CreateResultObject(subtitle);
-                                    MelonLogger.Msg("Setting parent for result");
-                                    if (result == null) MelonLogger.Error("Failed to get result.");
-                                    if (result.transform == null) MelonLogger.Error("Failed to get result's transform");
-                                    if (_listContent == null) MelonLogger.Error("Somehow list content is now null.");
-                                    if (_listContent.transform == null) MelonLogger.Error("List content's transform is null");
-                                    VRCCC.MainThreadExecutionQueue.Add( () => { 
-                                        result.transform.SetParent(_listContent.transform, false);
-                                        MelonLogger.Msg("parent set.");
-                                    });
-                                } catch (Exception e) { 
-                                    MelonLogger.Error($"Exception when trying to create a result object {e}");
-                                } 
-                                
-                            }
-                        }
-                    } catch (Exception e) { 
-                        MelonLogger.Error($"Exception when trying to get new subtitles. {e}.");
-                    }
+            if (!_initSucc) return;
+            _searchButton.onClick.AddListener((Action) SearchButtonOnClick);
+            _negativeSButton.onClick.AddListener( (Action)    ( () => { OffsetButtonClick(-1000); }));
+            _negativeHalfSButton.onClick.AddListener((Action) ( () => { OffsetButtonClick(-500); }));
+            _positiveHalfSButton.onClick.AddListener((Action) ( () => { OffsetButtonClick(500); }));
+            _positiveSButton.onClick.AddListener((Action)     ( () => { OffsetButtonClick(1000); }));
+        }
+        
+        private async void OffsetButtonClick(int offset) { 
+            VRCCC.TrackedPlayers[0].IncrementOrDecrementOffset(offset);
+            _currentOffsetText.text = VRCCC.TrackedPlayers[0].GetCurrentOffsetMs().ToString();
+        }
+        
+        private async void SearchButtonOnClick() { 
+            if (_inputField.text != "") { 
+                // TODO: fix clearing previous list 
+                //ClearResultList()
+                MelonLogger.Msg($"Searching for {_inputField.text}");
+                try {
+                    _subtitle = await SubtitlesApi.QuerySubtitle(_inputField.text, true);
+                    MelonLogger.Msg($"{_subtitle.Alternatives.Count} results in the list of alternatives.");
+                    foreach (Subtitle subtitle in _subtitle.Alternatives.Take(3))
+                        SetupResultAndSetParent(subtitle);
+                } catch (Exception e) { 
+                    MelonLogger.Error($"Exception when trying to get new subtitles. {e}.");
                 }
-            }));
-            
+            }
+        }
+        
+        private void SetupResultAndSetParent(Subtitle subtitle) { 
+            if (subtitle != null) { 
+                MelonLogger.Msg($"Handling returned result for {subtitle.MovieName}.");
+                try { 
+                    GameObject result = CreateResultObject(subtitle);
+                    VRCCC.MainThreadExecutionQueue.Add( () => { 
+                        result.transform.SetParent(_listContent.transform, false);
+                        MelonLogger.Msg("Parent set.");
+                    });
+                } catch (Exception e) { 
+                    MelonLogger.Error($"Exception when trying to create a result object {e}");
+                } 
+                
+            }
         }
         
         private void ClearResultList() { 
-            if (_listContent == null || _listContent.transform == null) return;
+            if (!_initSucc) return;
             // Leave the first item, which is used as a template and duplicated 
-            for (int x=_listContent.transform.childCount - 1; x>1; ++x) { 
+            for (int x=_listContent.transform.childCount - 1; x>1; ++x)
                 GameObject.Destroy(_listContent.transform.GetChild(x).gameObject);
-            }
         }
        
         /**
@@ -164,17 +178,10 @@ namespace VRCCC.QuickMenu
         
         private async void SelectButtonClick(Subtitle subtitle) { 
             if (subtitle == null) return;
-            MelonLogger.Msg($"Clicked select button for {subtitle.MovieName}!");
-                
+            
             string srtString = await SubtitlesApi.FetchSub(subtitle.SubDownloadLink);
             List<TimelineEvent> timelineEvents = SRTDecoder.DecodeSrtIntoTimelineEvents(srtString);
-            
-            foreach (TrackedPlayer player in VRCCC.TrackedPlayers) { 
-                MelonLogger.Msg("Swapping to new timeline!");
-                player.UnsafeSwapTimeline(subtitle.MovieName, timelineEvents);
-                MelonLogger.Msg($"Timeline has {timelineEvents.Count} events.");
-            }
-            
+            VRCCC.TrackedPlayers[0].UnsafeSwapTimeline(subtitle.MovieName, timelineEvents);
         }
         
     }
